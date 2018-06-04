d8fe0c1ca50b53e101f8173450dae9a73fca40ca
JethroSovr <i@sovr.me>
2018-06-04 17:26:02 +0800

    make file clean

--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -6,7 +6,7 @@ import * as userController from './user.controllers';
 import userValidation from './user.validations';
 
 const routes = new Router();
-//
+
 routes.post('/signup', validate(userValidation.signup), userController.signUp);
 routes.post('/login', authLocal, userController.login);
 
